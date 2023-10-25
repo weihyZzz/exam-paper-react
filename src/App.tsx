3d3ed3c81ecd322170dc83753de6c794e15b6191
weihy <971973918@qq.com>
2023-10-25 12:00:13 +0800

    chore: ts类型的事件监听写法

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import type { MouseEvent } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>, name: string) => {
+    event.preventDefault()
+    console.log('click.', name)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button
+          onClick={(e: MouseEvent<HTMLButtonElement>) => {
+            handleClick(e, 'weihy')
+          }}
+        >
+          Click
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
